bfba62d9a63b476bcb84dbc5ddb1bd8c5b0844d0
Tim Harding <timharding@Tims-MacBook-Pro.local>
2023-04-03 16:58:13 -0700

    Move resume form to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,22 @@
 import { useState } from 'react'
-import { TextField, Button, FileUpload, Radio, Checkbox } from './components'
+import { ResumeForm } from './ResumeForm'
+import { Button } from './components'
 
 function App() {
-  const [name, setName] = useState('')
-  const [message, setMessage] = useState('Not fetched')
-
-  const handleFetch = async () => {
-    if (!name) return setMessage('Not fetched')
-    const res = await fetch('/api/hello/' + name)
-    const { message: newMessage } = await res.json()
-    setMessage(newMessage)
-    setName('')
-  }
-
-  const [file, setFile] = useState<File>()
-  const [response, setResponse] = useState<JSON>()
-
-  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const uploadedFile: File | undefined = Array.from(e.target.files ?? [])[0]
-    setFile(uploadedFile)
-  }
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    if (!name || !file) return alert('Missing name and/or file')
-    const qs = new URLSearchParams({ name }).toString()
-    const url = '/api/new?' + qs
-    const body = new FormData()
-    body.append('file', file)
-    const response = await fetch(url, { method: 'POST', body }).then(res => res.json())
-    setResponse(response)
-    setName('')
-    setFile(undefined)
-  }
+  const [jokeResponse, setJokeResponse] = useState<JSON>()
 
   const handleGetJoke = async () => {
     const response = await fetch('/api/joke').then(res => res.json())
-    setResponse(response)
+    setJokeResponse(response)
   }
 
   return (
     <div className="w-full m-auto max-w-lg">
-      <form onSubmit={handleSubmit} className="mb-2 flex flex-col items-stretch gap-4">
-        <TextField
-          label="Name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          className="w-full"
-          required
-        />
-        <FileUpload
-          label="Resume"
-          file={file}
-          onRemove={() => setFile(undefined)}
-          onUpload={file => setFile(file)}
-          required
-          className="w-full"
-        />
-        <div className="flex items-center gap-2 ml-auto">
-          <Button variant="primary" size="medium" type="submit">
-            Submit
-          </Button>
-        </div>
-      </form>
+      <ResumeForm />
       <Button variant="secondary" size="large" type="button" onClick={handleGetJoke}>
         Tell me a joke
       </Button>
-      {response != null && <div dangerouslySetInnerHTML={{ __html: `${response}` }} />}
+      {jokeResponse != null && <div dangerouslySetInnerHTML={{ __html: `${jokeResponse}` }} />}
     </div>
   )
 }
--- src/ResumeForm/index.tsx
+++ src/ResumeForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { TextField, FileUpload, Button } from '../components'
 import styles from './styles.css'
 
 interface ResumeFormFields {
@@ -8,20 +9,12 @@ interface ResumeFormFields {
   details?: string
 }
 
-const isFieldRequired = (field: keyof ResumeFormFields): boolean => {
-  switch (field) {
-    case 'name':
-    case 'cv':
-      return true
-  }
-
-  return false
-}
-
 interface ResumeFormProps {}
 
 export const ResumeForm: React.FC<ResumeFormProps> = () => {
   const [fields, setFields] = useState<Partial<ResumeFormFields>>({})
+  const [error, setError] = useState<string>()
+  const [response, setResponse] = useState<JSON>()
 
   const handleField = (
     field: keyof ResumeFormFields,
@@ -32,5 +25,44 @@ export const ResumeForm: React.FC<ResumeFormProps> = () => {
 
   const canSubmit = fields.name != null && fields.cv != null
 
-  return null
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (!canSubmit) {
+      setError('Missing required fields')
+      response != null && setResponse(undefined)
+    }
+    const qs = new URLSearchParams({ name: fields.name! }).toString()
+    const url = '/api/new?' + qs
+    const body = new FormData()
+    body.append('file', fields.cv!)
+    const apiResponse = await fetch(url, { method: 'POST', body }).then(res => res.json())
+    setResponse(apiResponse)
+    setFields({})
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="mb-2 flex flex-col items-stretch gap-4">
+      <TextField
+        label="Name"
+        name="name"
+        value={fields.name ?? ''}
+        onChange={e => handleField('name', e.target.value)}
+        className="w-full"
+        required
+      />
+      <FileUpload
+        label="Resume"
+        file={typeof fields.cv === 'string' ? undefined : fields.cv}
+        onRemove={() => handleField('cv', undefined)}
+        onUpload={file => handleField('cv', file)}
+        className="w-full"
+        required
+      />
+      <div className="flex items-center gap-2 ml-auto">
+        <Button variant="primary" size="medium" type="submit">
+          Submit
+        </Button>
+      </div>
+    </form>
+  )
 }
